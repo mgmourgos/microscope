64226960be4e0bd8014e9021ffb39e7a9cecfee9
mgmourogs@users.noreply.github.com <mgmourgos@users.noreply.github.com>
2017-03-24 19:12:35 -0700

    Adds Comments

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,10 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+//Remember in router.js we wait on these subs before doing anything.
